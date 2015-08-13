86cd4bd371104fafcd0272949b687afbbaf136fc
faribas <seyedzadehkhanshan.f@husky.neu.edu>
2015-08-12 22:48:04 -0400

    rules.py is updated with calculated kinetic values

--- input/kinetics/families/Intra_RH_Add_Exocyclic/rules.py
+++ input/kinetics/families/Intra_RH_Add_Exocyclic/rules.py
@@ -4,39 +4,24 @@
 name = "Intra_RH_Add_Exocyclic/rules"
 shortDesc = u""
 longDesc = u"""
-<<<<<<< HEAD
-=======
-"""
-recommended = True
->>>>>>> b60bf41336e4a52f6397f18b7d4e90d4d41af42f
 
 """
 entry(
     index = 807,
     label = "Rn;multiplebond_intra;radadd_intra",
     kinetics = ArrheniusEP(
-        A = (1e+10, 's^-1'),
+        A = (10000000000.0, 's^-1'),
         n = 0,
         alpha = 0,
         E0 = (5, 'kcal/mol'),
         Tmin = (300, 'K'),
         Tmax = (1500, 'K'),
     ),
+    reference = None,
+    referenceType = "",
     rank = 0,
-<<<<<<< HEAD
-=======
     shortDesc = u"""""",
->>>>>>> b60bf41336e4a52f6397f18b7d4e90d4d41af42f
     longDesc = 
 u"""
-To make this family possible, rate parameters of head node groups are estimated from typical threshold amounts of 
-A (10^9 - 10^13 s^-1) and E0 (5-50 kcal/mol) in unimolecular gas-phase reactions. 
-Definitely better kinetic parameters are needed to fill this database.
 """,
-<<<<<<< HEAD
-)
-
-=======
-    
-)
->>>>>>> b60bf41336e4a52f6397f18b7d4e90d4d41af42f
+)    
